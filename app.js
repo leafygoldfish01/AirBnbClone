ce1021e489c298365acf1308ef213e1c2f5a4851
leafygoldfish01 <gaurav.31103@gmail.com>
2024-11-28 00:13:17 +0530

    Added MiddleWares and Error Handling

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("../MajorProject/models/listing.js");
+const Listing = require("../AirBnbClone/models/listing.js");
 const path=require("path");
+const { isValidObjectId } = mongoose;
+const methodOverride= require("method-override");
+const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchmea}=require("./schema.js")
 
 const mongooseurl="mongodb://127.0.0.1:27017/majorproject";
 
@@ -19,34 +25,85 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 //Indexx route
-app.get("/listings", async(req,res)=>{
+app.get("/listings", wrapAsync(async(req,res)=>{
     const allisting= await Listing.find({});
     res.render("listings/index.ejs",{allisting});
-});
+}));
 
 //New Route
-app.get("/listings/new",async(req,res)=>{
+app.get("/listings/new",wrapAsync(async(req,res)=>{
     res.render("listings/new.ejs");
-});
+}));
 
-//Show Route
+const validListing= (req,res,next)=>{
+    let {error}=listingSchmea.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }
+}
+
 app.get("/listings/:id",async(req,res)=>{
     let {id} = req.params;
+    if (!isValidObjectId(id)) {
+        return res.status(400).send("Invalid ID format");
+    }
     const listing = await Listing.findById(id);
+    if (!listing) {
+        return res.status(404).send("Listing not found");
+    }
     res.render("listings/show.ejs",{listing});
 });
 
-//Create Route
-app.post("/listings", async(req,res)=>{
-    const newlisting = new Listing(req.body.listing);
-    await newlisting.save();
+//Edit Route
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+}));
+
+//Update Route
+app.put("/listings/:id", wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send valid data for listing");
+    }
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+}));
+
+//Delete Route
+app.delete("/listings/:id", wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    let deletedlisting= await Listing.findByIdAndDelete(id);
+    console.log(deletedlisting);
     res.redirect("/listings");
+}));
+
+//Create Route
+app.post("/listings", wrapAsync(async(req,res,next)=>{
+        const newlisting = new Listing(req.body.listing);
+        await newlisting.save();
+        res.redirect("/listings");
+}));
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"));
+});
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something Went Wrong!"}=err;
+    res.render("listing/error.ejs",{message});
+    // res.status(statusCode).send(message);
 });
 
 
--- models/listing.js
+++ models/listing.js
@@ -2,20 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title : {
-        type:String,
-        required : true
+    title: {
+        type: String,
+        required: true
     },
     description: String,
-    picture : {
+    image: {
         type: String,
-        default : "https://unsplash.com/photos/multicolored-hallway-RnCPiXixooY",
-        set :(v) => v===""? "https://unsplash.com/photos/multicolored-hallway-RnCPiXixooY":v
+        default: "https://unsplash.com/photos/multicolored-hallway-RnCPiXixooY",
+        set: (v) => {
+            if (v && v.url) {
+                return v.url;
+            }
+            return v === "" ? "https://unsplash.com/photos/multicolored-hallway-RnCPiXixooY" : v;
+        }
     },
-    price : Number,
-    location : String,
-    country : String,
+    price: Number,
+    location: String,
+    country: String,
 });
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
